16e70c2fa4dc5bf3cd1940bdbbd0f214176c49c0
Alex <Sashachelmodeev@gmail.com>
2022-12-07 09:54:03 +0300

    Commit

--- src/lesson_12/HM/HM3.java
+++ src/lesson_12/HM/HM3.java
@@ -8,22 +8,26 @@ public class HM3 {
     public static void main(String[] args) throws IOException {
         Scanner reader = new Scanner(System.in);
 
-        Map<String, String> map = new HashMap<>();
-        while (true) {
-            String city = reader.next();
-            if (city.isEmpty()) {
-                break;
-            }
-            String family = reader.next();
+        Map<String, String> map = new HashMap<String, String>();
 
-            map.put(city, family);
+        while (true)
+        {
+            String sity1 = reader.next();
+            if (sity1.isEmpty())
+                break;
+            else {
+                String lastName = reader.next();
+                map.put(sity1, lastName);
+            }
         }
 
-        String city = reader.next();
+        String cityKeyboard = reader.next();
 
-        if (map.containsKey(city)) {
-            String familyName = map.get(city);
-            System.out.println(familyName);
+        for (Map.Entry<String, String> pair : map.entrySet())
+        {
+            if (pair.getKey().equals(cityKeyboard))
+                System.out.println(pair.getValue());
         }
     }
 }
+
